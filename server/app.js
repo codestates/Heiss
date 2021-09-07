a2bddc7445c8a278ff8947bfedffcfbb6a991e14
yun sungmin <smy07207@gmail.com>
2021-09-07 17:48:51 +0900

    서버 초기 설정

---
+++ server/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const https = require("https");
+const cors = require("cors");
+const fs = require("fs");
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(
+	cors({
+		origin: true,
+		credentials: true,
+		methods: ["GET", "POST", "OPTIONS"],
+	})
+);
+
+app.get("/", (req, res) => {
+	res.send("hello world~~~");
+});
+const HTTPS_PORT = process.env.HTTPS_PORT || 50;
+
+let server;
+if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+	const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
+	const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
+	const credentials = { key: privateKey, cert: certificate };
+
+	server = https.createServer(credentials, app);
+	server.listen(HTTPS_PORT, () => console.log("https server runnning"));
+} else {
+	server = app.listen(HTTPS_PORT, () => console.log("http server running "));
+}
