29e40fad7b951093496399b0f508cbde270b75a5
yeeun62 <byebye62@naver.com>
2021-09-10 13:31:16 +0900

    [Add] review api 전체리뷰, 특정리뷰 작성

--- server/route/review.js
+++ server/route/review.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers");
 
-router.get("/", controller.review.allReview);
-router.post("/", controller.review.writeReview);
+router.get("/", controller.review.getAllReview);
+router.get("/:id", controller.review.getDetailReview);
+router.get("/:id/like", controller.review.getLikeReview);
+
+router.post("/", controller.review.postWriteReview);
+
+router.patch("/:id", controller.review.patchEditReview);
+
+router.delete("/:id", controller.review.deleteReview);
 
 module.exports = router;
