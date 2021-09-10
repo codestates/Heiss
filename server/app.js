23fc7eff345505fafbbde6d70113e8fc1df993f9
yeeun62 <byebye62@naver.com>
2021-09-10 10:04:35 +0900

    [Add] review api 작성중

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const cors = require("cors");
-const userRouter = require("./route/user");
+const morgan = require("morgan");
+const multer = require("multer");
+const form_data = multer();
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
+const userRouter = require("./route/user");
+const reviewRouter = require("./route/review");
 
 app.use(express.json());
+app.use(morgan("tiny"));
+app.use(form_data.array());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(
 	cors({
 		origin: true,
@@ -14,12 +23,13 @@ app.use(
 );
 
 app.use("/user", userRouter);
+app.use("/review", reviewRouter);
 
 app.get("/", (req, res) => {
 	res.send("hello world~~~");
 });
-const PORT = 80;
 
+const PORT = 80;
 const server = app.listen(PORT, () => console.log("서버가 열려따..!"));
 
 module.exports = server;
