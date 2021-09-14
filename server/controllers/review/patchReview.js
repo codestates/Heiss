a25cda872e84b4318048953b1882631b6e86b188
yeeun62 <byebye62@naver.com>
2021-09-14 17:32:44 +0900

    [Refactor] review api 전면 수정

--- server/controllers/review/patchReview.js
+++ server/controllers/review/patchReview.js
@@ -1,35 +1,37 @@
-const { like, review, source, users } = require("../../models");
+const { review, source } = require("../../models");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-module.exports = {
-	patchReview: async (req, res) => {
-		const reviewId = req.params.id;
-		const { score, title, desc, caseId } = req.body;
-		const accessToken = req.cookies.accessToken;
-		const imgUrl = req.files.location;
-		try {
-			const userInfo = await jwt.verify(accessToken, process.env.ACCESS_SECRET);
-			if (userInfo.id === reviewId.userId) {
-				try {
-					if (score && title && desc && caseId) {
-						await review.update(
-							{ score, title, desc, caseId },
-							{
-								where: { id: reviewId },
-							}
-						);
-						res.status(200).json({ message: "ok" });
-					}
-				} catch (err) {
-					res.status(422).json({ message: "insufficient parameters supplied" });
-				}
-				if (imgUrl) {
-					const addSource = await source.bulkCreate([{ reviewId }, { imgUrl }]);
-				}
+module.exports = async (req, res) => {
+	const reviewId = req.params.id;
+	const { score, title, desc, caseId } = req.body;
+	const accessToken = req.cookies.accessToken;
+	const imgUrl = req.files.location;
+	try {
+		const userInfo = await jwt.verify(accessToken, process.env.ACCESS_SECRET);
+		const findReview = await review.findOne({ where: { id: reviewId } });
+		if (!findReview) {
+			res.status(404).json({ message: "Not found post" });
+		}
+		if (userInfo.id === findReview.dataValues.userId) {
+			if (imgUrl) {
+				const addSource = await source.bulkCreate([{ reviewId }, { imgUrl }]);
 			}
-		} catch (err) {
+			if (score && title && desc && caseId) {
+				await review.update(
+					{ score, title, desc, caseId },
+					{
+						where: { id: reviewId },
+					}
+				);
+				res.status(200).json({ message: "ok" });
+			} else {
+				res.status(422).json({ message: "insufficient parameters supplied" });
+			}
+		} else {
 			res.status(401).json({ message: "Unauthorized request" });
 		}
-	},
+	} catch (err) {
+		res.status(500).send(console.log(err));
+	}
 };
