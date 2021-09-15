d0e6015c477e52f634e6456ebf3cf263842b01d0
yeeun62 <byebye62@naver.com>
2021-09-16 01:27:59 +0900

    [Add] cart api 작성

--- server/controllers/cart/postCart.js
+++ server/controllers/cart/postCart.js
@@ -5,6 +5,9 @@ require("dotenv").config();
 module.exports = async (req, res) => {
 	const caseId = req.body.caseId;
 	const accessToken = req.cookies.accessToken;
+	if (!accessToken) {
+		res.status(401).json({ message: "please log in" });
+	}
 	try {
 		const userInfo = await jwt.verify(accessToken, process.env.ACCESS_SECRET);
 		const findCase = await customCase.findOne({
@@ -14,7 +17,7 @@ module.exports = async (req, res) => {
 			await customCase.update({ cart: true }, { where: { id: caseId } });
 			res.status(200).json({ message: "ok" });
 		} else {
-			res.status(401).json({ message: "Unauthorized request" });
+			res.status(404).json({ message: "Not found" });
 		}
 	} catch (err) {
 		res.status(500).send(console.log(err));
