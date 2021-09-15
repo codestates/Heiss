b7311990464a9d53c2dfeeb74bc6459c1ba6cad5
yeeun62 <byebye62@naver.com>
2021-09-15 10:37:46 +0900

    [Add] merge

--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -3,11 +3,7 @@ const bcrypt = require("bcrypt");
 
 module.exports = async (req, res) => {
 	const { email, username, password, provider } = req.body;
-<<<<<<< HEAD
-	// console.log(req.file);
-=======
 	// console.log(req.files);
->>>>>>> 306594b39de65fdee8a7f658aac8886fb4c1b208
 	let user = await model.users.findOne({ where: { email: email } });
 	if (user) {
 		//email 중복
@@ -18,21 +14,13 @@ module.exports = async (req, res) => {
 				if (err) {
 					throw err;
 				} else {
-<<<<<<< HEAD
-					// const image = fs.readFileSync(req.file.path);
-=======
->>>>>>> 306594b39de65fdee8a7f658aac8886fb4c1b208
 					model.users
 						.create({
 							userName: username,
 							email: email,
 							provider: provider,
 							password: hash,
-<<<<<<< HEAD
-							// profileImg: image,
-=======
 							profileImg: req.file.location,
->>>>>>> 306594b39de65fdee8a7f658aac8886fb4c1b208
 						})
 						.then((result) => {
 							res.status(200).send();
