dff8fd95a2577e4ce8ae0167d906c88c04ebc291
JangSeokJin1228 <heroes30030@gmail.com>
2021-10-06 10:25:29 +0900

    alert창 미완성....

--- client/src/modal/AlertModal.js
+++ client/src/modal/AlertModal.js
@@ -1,49 +1,56 @@
-import React, { useState } from "react";
-const AlertModal = (props) => {
-	const { open, close, alertString, alertBtn } = props;
-	// const [open, setOpen] = useState(false);
+// import React, { useState, useEffect } from "react";
+// import styled from "styled-components";
 
-	// 	const handleClick = () => {
-	// 		setOpen(true);
-	// 	};
+// const SignSection = styled.div`
+// 	display: flex;
+// 	flex-direction: column;
+// 	justify-content: center;
+// 	align-items: center;
+// 	text-align: center;
+// 	width: 100%;
+// 	height: 100%;
 
-	return (
-		<div className="modalcontainer stopdragging">
-			{open ? (
-				<div className="modalbackdrop">
-					<div className="modalview">
-						<div className="desc">
-							{alertString.split("\n").map((line) => (
-								<span key={line}>
-									{line}
-									<br />
-								</span>
-							))}
-							<button className="closebtn" onClick={close}>
-								{alertBtn}
-							</button>
-						</div>
-					</div>
-				</div>
-			) : null}
-		</div>
-	);
-};
-export default AlertModal;
+// 	img {
+// 		height: 6rem;
 
-// export default Alert;
+// 		@media ${(props) => props.theme.mobileL} {
+// 			height: 4rem;
+// 		}
+// 	}
 
-// import React from "react";
-// import { SnackbarProvider } from "notistack";
-// import { useSnackbar } from "notistack";
+// 	@media ${(props) => props.theme.mobileL} {
+// 		padding: 0.4rem;
+// 	}
+// `;
+// const ModalBtn = styled.button`
+// 	background-color: #4000c7;
+// 	text-decoration: none;
+// 	border: none;
+// 	padding: 20px;
+// 	color: white;
+// 	border-radius: 30px;
+// 	cursor: grab;
+// `;
 
 // const AlertModal = () => {
-// 	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+// 	const [isOpen, setIsOpen] = useState(false);
+// 	const handleModal = () => {
+// 		setIsOpen(!isOpen);
+// 	};
+
 // 	return (
-// 		<SnackbarProvider>
-// 			<div className="App">
-// 				<button onClick={() => enqueueSnackbar("로그인이 되었습니다")}></button>
-// 			</div>
-// 		</SnackbarProvider>
+// 		<AlertModal>
+// 			<ModalBtn></ModalBtn>
+// 		</AlertModal>
 // 	);
 // };
+
+// export default AlertModal;
+import React, { useState } from "react";
+
+const AlertModal = () => {
+	const [action, setAction] = useState("");
+	return;
+};
+
+export default AlertModal;
